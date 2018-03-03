b5baf998a520fa982366ef12497bc9c383ea8b41
HeskeyBaozi <hezhiyu233@foxmail.com>
2018-03-03 14:33:46 +0800

    type-safe menu component

--- src/components/menu/GeneralMenu/index.tsx
+++ src/components/menu/GeneralMenu/index.tsx
@@ -1,6 +1,6 @@
 import { Menu } from 'antd';
 import { ClickParam } from 'antd/es/menu';
-import { computed } from 'mobx';
+import { computed, expr, extendObservable } from 'mobx';
 import { inject, observer } from 'mobx-react';
 import pathToRegexp from 'path-to-regexp';
 import React from 'react';
@@ -18,95 +18,86 @@ export interface IGeneralMenuItem {
   title: React.ReactNode;
 }
 
-interface IGeneralMenuProps<P> extends RouteConfigComponentProps<P> {
-  $Global?: IGlobalStore;
-  dataSource: IGeneralMenuItem[];
+interface ICreateMenu<P> {
+  dataSource: IGeneralMenuItem[] | ((props: P) => IGeneralMenuItem[]);
   returnTo?: (props: RouteConfigComponentProps<P>) => string;
 }
 
-@inject('$Global')
-@observer
-export default class GeneralMenu<P> extends React.Component<IGeneralMenuProps<P>> {
+interface IGeneralMenuProps<P> extends RouteConfigComponentProps<P> {
+  $Global?: IGlobalStore;
+}
 
-  @computed
-  get ReturnTo() {
-    const { returnTo } = this.props;
-    return returnTo ? (
-      <Item className={ styles.returnItem } key={ 'RETURN' }>
-        <IconText icon={ 'rollback' } title={ '返回上一级' } />
-      </Item>
-    ) : null;
-  }
+export default function createMenu<P>({ dataSource, returnTo }: ICreateMenu<P>)
+  : React.ComponentType<IGeneralMenuProps<P> | {}> {
+  return inject('$Global')(observer(
+    class InnerMenu extends React.Component<IGeneralMenuProps<P>> {
+      dataSourceWithRegExp: Array<{ url: string, reg: RegExp }>;
+      List: React.ReactNode;
+      ReturnTo: React.ReactNode | null;
+      constructor(props: IGeneralMenuProps<P> | {}) {
+        super(props as IGeneralMenuProps<P>);
+        extendObservable(this, {
+          get ReturnTo() {
+            return returnTo ? (
+              <Item className={ styles.returnItem } key={ 'RETURN' }>
+                <IconText icon={ 'rollback' } title={ '返回上一级' } />
+              </Item>
+            ) : null;
+          },
+          get source() {
+            const { match } = props as IGeneralMenuProps<P>;
+            if (typeof dataSource === 'function') {
+              return dataSource(match.params);
+            }
+            return dataSource;
+          },
+          get List() {
+            return this.source.map(({ url, icon, title }) => (
+              <Item key={ url }>
+                <IconText icon={ icon } title={ title } />
+              </Item>
+            ));
+          },
+          get dataSourceWithRegExp() {
+            return this.source.map(({ url, path }) => ({ url, reg: pathToRegexp(path || url) }));
+          }
+        });
+      }
 
-  @computed
-  get List() {
-    const { dataSource } = this.props;
-    return dataSource.map(({ url, icon, title }) => (
-      <Item key={ url }>
-        <IconText icon={ icon } title={ title } />
-      </Item>
-    ));
-  }
+      navigate = ({ key: url }: ClickParam) => {
+        const { $Global, ...restProps } = this.props;
+        const { history } = restProps;
+        if (url === 'RETURN' && returnTo) {
+          history.push(returnTo(restProps));
+        } else {
+          history.push(url);
+        }
+      }
 
-  @computed
-  get dataSourceWithRegExp() {
-    const { dataSource } = this.props;
-    return dataSource.map(({ url, path }) => ({ url, reg: pathToRegexp(path || url) }));
-  }
-
-  @computed
-  get selectedKeys() {
-    const { location } = this.props;
-    return this.dataSourceWithRegExp
-      /**
-       * convert url(path) into regexp and test it to get current selected keys...
-       */
-      .filter(({ reg }) => reg.test(location.pathname))
-      .map(({ url }) => url);
-  }
-
-  navigate = ({ key: url }: ClickParam) => {
-    const { $Global, dataSource, returnTo, ...restProps } = this.props;
-    const { history } = restProps;
-    if (url === 'RETURN' && returnTo) {
-      history.push(returnTo(restProps));
-    } else {
-      history.push(url);
+      render() {
+        const { $Global, location } = this.props;
+        const selectedKeys = expr(() => {
+          return this.dataSourceWithRegExp
+            /**
+             * convert url(path) into regexp and test it to get current selected keys...
+             */
+            .filter(({ reg }) => reg.test(location.pathname))
+            .map(({ url }) => url);
+        });
+        return (
+          <Menu
+            className={ styles.menu }
+            theme={ 'dark' }
+            inlineCollapsed={ $Global!.collapsed }
+            onClick={ this.navigate }
+            mode={ 'inline' }
+            selectedKeys={ selectedKeys }
+          >
+            { this.ReturnTo }
+            { this.List }
+          </Menu>
+        );
+      }
     }
-  }
-
-  render() {
-    const { $Global } = this.props;
-    return (
-      <Menu
-        className={ styles.menu }
-        theme={ 'dark' }
-        inlineCollapsed={ $Global!.collapsed }
-        onClick={ this.navigate }
-        mode={ 'inline' }
-        selectedKeys={ this.selectedKeys }
-      >
-        { this.ReturnTo }
-        { this.List }
-      </Menu>
-    );
-  }
-}
-
-interface IGeneralMenuGeneratorProps<P> {
-  dataSource: IGeneralMenuItem[];
-  returnTo?: <T = P>(props: RouteConfigComponentProps<T>) => string;
-}
-
-export function createMenu<P>({
-                                dataSource,
-                                returnTo
-                              }: IGeneralMenuGeneratorProps<P>) {
-  return ((props: RouteConfigComponentProps<P>) => (
-    <GeneralMenu
-      dataSource={ dataSource }
-      returnTo={ returnTo }
-      { ...props }
-    />
-  )) as React.SFC<RouteConfigComponentProps<P> | {}>;
+  ));
 }
--- src/pages/Main/router.tsx
+++ src/pages/Main/router.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Redirect } from 'react-router';
 import { RouteConfig } from 'react-router-config';
 import { IRouteConfigWithBreadcrumb } from '../../components/common/PageHeader/interfaces';
-import { createMenu } from '../../components/menu/GeneralMenu';
+import createMenu from '../../components/menu/GeneralMenu';
 import { dynamic } from '../../utils/dynamic';
 import { routes as coursesRoutes } from './Courses/router';
 import { routes as homeRoutes } from './Home/router';
@@ -23,6 +23,11 @@ export const routes: IRouteConfigWithBreadcrumb[] = [
     routes: coursesRoutes
   },
   {
+    path: '/course/:course_id',
+    component: () => <div>OneCourse</div>,
+    breadcrumbName: '课程'
+  },
+  {
     path: '/notification',
     breadcrumbName: '所有通知',
     component: dynamic(() => import('./Notification')),
@@ -35,6 +40,17 @@ export const routes: IRouteConfigWithBreadcrumb[] = [
 ];
 
 export const menuRoutes: RouteConfig[] = [
+  {
+    path: '/course/:course_id',
+    component: createMenu<{ course_id: string }>({
+      dataSource: ({ course_id }) => ([
+        { url: `/course/${course_id}/home`, icon: 'home', title: '概览' },
+        { url: `/course/${course_id}/assignments`, icon: 'edit', title: '作业' },
+        { url: `/course/${course_id}/discussions`, icon: 'coffee', title: '讨论' }
+      ]),
+      returnTo: () => '/courses'
+    })
+  },
   {
     path: '/',
     component: createMenu<{}>({
